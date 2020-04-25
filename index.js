235a9611b8a85686d3e5ea09e63e415c1bf54083
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2020-04-25 15:36:29 +1200

    Added onUserSignUp function to add manufacturer on new manufacturer sign up (#29)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const { createServer } = require('./dist');
+const { createServer, setupContainer } = require('./dist');
 
 admin.initializeApp();
 
@@ -8,6 +8,21 @@ const graphql = functions.https.onRequest((request, response) => {
   createServer().then(server => server(request, response));
 });
 
+const onUserSignUp = functions.firestore.document('/user/{userId}').onCreate((snapshot, context) => {
+      const  { userType } = snapshot.data();
+
+       if (userType !== 'manufaturer') {
+         return;
+       }
+
+      const userId = context.params.userId;
+      const container = setupContainer({user_id: userId});
+      const manufacturerBusinessService = container.resolve('manufacturerBusinessService')
+
+      manufacturerBusinessService.create({userId, name: "no name set yet!!!"});
+});
+
 module.exports = {
-  graphql
+  graphql,
+  onUserSignUp
 };
